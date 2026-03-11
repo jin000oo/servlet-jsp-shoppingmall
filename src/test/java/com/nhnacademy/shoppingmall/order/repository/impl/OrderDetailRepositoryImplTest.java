a9b58896464fbde7417255aad48fda00ea6cf5f6
jin000oo <wlsdud3309@naver.com>
2026-03-11 18:42:03 +0900

    test: order repository, service 테스트 코드 추가

---
+++ src/test/java/com/nhnacademy/shoppingmall/order/repository/impl/OrderDetailRepositoryImplTest.java
@@ -0,0 +1,117 @@
+/*
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * + Copyright 2026. NHN Academy Corp. All rights reserved.
+ * + * While every precaution has been taken in the preparation of this resource,  assumes no
+ * + responsibility for errors or omissions, or for damages resulting from the use of the information
+ * + contained herein
+ * + No part of this resource may be reproduced, stored in a retrieval system, or transmitted, in any
+ * + form or by any means, electronic, mechanical, photocopying, recording, or otherwise, without the
+ * + prior written permission.
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ */
+
+package com.nhnacademy.shoppingmall.order.repository.impl;
+
+import com.nhnacademy.shoppingmall.common.mvc.transaction.DbConnectionThreadLocal;
+import com.nhnacademy.shoppingmall.order.domain.Order;
+import com.nhnacademy.shoppingmall.order.domain.OrderDetail;
+import com.nhnacademy.shoppingmall.order.repository.OrderDetailRepository;
+import com.nhnacademy.shoppingmall.order.repository.OrderRepository;
+import com.nhnacademy.shoppingmall.product.domain.Category;
+import com.nhnacademy.shoppingmall.product.domain.Product;
+import com.nhnacademy.shoppingmall.product.repository.CategoryRepository;
+import com.nhnacademy.shoppingmall.product.repository.ProductRepository;
+import com.nhnacademy.shoppingmall.product.repository.impl.CategoryRepositoryImpl;
+import com.nhnacademy.shoppingmall.product.repository.impl.ProductRepositoryImpl;
+import com.nhnacademy.shoppingmall.user.domain.User;
+import com.nhnacademy.shoppingmall.user.repository.UserRepository;
+import com.nhnacademy.shoppingmall.user.repository.impl.UserRepositoryImpl;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class OrderDetailRepositoryImplTest {
+
+    OrderDetailRepository orderDetailRepository = new OrderDetailRepositoryImpl();
+    OrderRepository orderRepository = new OrderRepositoryImpl();
+    UserRepository userRepository = new UserRepositoryImpl();
+    ProductRepository productRepository = new ProductRepositoryImpl();
+    CategoryRepository categoryRepository = new CategoryRepositoryImpl();
+
+    OrderDetail testOrderDetail;
+    Order testOrder;
+    User testUser;
+    Product testProduct;
+    Category testCategory;
+
+    @BeforeEach
+    void setUp() {
+        DbConnectionThreadLocal.initialize();
+
+        testUser = new User(
+                "test-user",
+                "nhn아카데미",
+                "nhnacademy-test-password",
+                "19900505",
+                User.Auth.ROLE_USER,
+                100_0000,
+                LocalDateTime.now(),
+                null
+        );
+
+        testOrder = new Order(
+                "test-order",
+                "test-user",
+                10_000
+        );
+
+        testCategory = new Category(
+                "test-category",
+                "test-category-name",
+                1
+        );
+
+        List<String> categories = new ArrayList<>();
+        categories.add("test-category");
+
+        testProduct = new Product(
+                "test-product",
+                "test-product-name",
+                10_000,
+                500,
+                categories
+        );
+
+        testOrderDetail = new OrderDetail(
+                "test-order-detail",
+                "test-order",
+                "test-product",
+                10
+        );
+
+        userRepository.save(testUser);
+        categoryRepository.save(testCategory);
+        productRepository.save(testProduct);
+        orderRepository.save(testOrder);
+    }
+
+    @AfterEach
+    void tearDown() {
+        DbConnectionThreadLocal.setSqlError(true);
+        DbConnectionThreadLocal.reset();
+    }
+
+    @Test
+    @DisplayName("주문 내역 저장")
+    void save() {
+        int result = orderDetailRepository.save(testOrderDetail);
+
+        Assertions.assertEquals(1, result);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/shoppingmall/order/service/impl/OrderServiceImplTest.java
@@ -0,0 +1,139 @@
+/*
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * + Copyright 2026. NHN Academy Corp. All rights reserved.
+ * + * While every precaution has been taken in the preparation of this resource,  assumes no
+ * + responsibility for errors or omissions, or for damages resulting from the use of the information
+ * + contained herein
+ * + No part of this resource may be reproduced, stored in a retrieval system, or transmitted, in any
+ * + form or by any means, electronic, mechanical, photocopying, recording, or otherwise, without the
+ * + prior written permission.
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ */
+
+package com.nhnacademy.shoppingmall.order.service.impl;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.nhnacademy.shoppingmall.order.domain.Order;
+import com.nhnacademy.shoppingmall.order.domain.OrderDetail;
+import com.nhnacademy.shoppingmall.order.exception.InsufficientAmountException;
+import com.nhnacademy.shoppingmall.order.exception.InsufficientQuantityException;
+import com.nhnacademy.shoppingmall.order.repository.OrderDetailRepository;
+import com.nhnacademy.shoppingmall.order.repository.OrderRepository;
+import com.nhnacademy.shoppingmall.order.service.OrderService;
+import com.nhnacademy.shoppingmall.point.service.PointService;
+import com.nhnacademy.shoppingmall.product.domain.Product;
+import com.nhnacademy.shoppingmall.product.repository.ProductRepository;
+import com.nhnacademy.shoppingmall.thread.channel.RequestChannel;
+import com.nhnacademy.shoppingmall.thread.request.impl.PointChannelRequest;
+import com.nhnacademy.shoppingmall.user.domain.User;
+import com.nhnacademy.shoppingmall.user.repository.UserRepository;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class OrderServiceImplTest {
+
+    OrderRepository orderRepository = Mockito.mock(OrderRepository.class);
+    OrderDetailRepository orderDetailRepository = Mockito.mock(OrderDetailRepository.class);
+    UserRepository userRepository = Mockito.mock(UserRepository.class);
+    ProductRepository productRepository = Mockito.mock(ProductRepository.class);
+    PointService pointService = Mockito.mock(PointService.class);
+    RequestChannel requestChannel = Mockito.mock(RequestChannel.class);
+    OrderService orderService = new OrderServiceImpl(
+            orderRepository, orderDetailRepository, userRepository, productRepository, pointService, requestChannel);
+
+    User testUser = new User(
+            "test-user",
+            "nhn아카데미",
+            "nhnacademy-test-password",
+            "19900505",
+            User.Auth.ROLE_USER,
+            100_0000,
+            LocalDateTime.now(),
+            null
+    );
+
+    Order testOrder = new Order(
+            "test-order",
+            "test-user",
+            100_000
+    );
+
+    OrderDetail testOrderDetail = new OrderDetail(
+            "test-order-detail",
+            "test-order",
+            "test-product",
+            100
+    );
+
+    Product testProduct = new Product(
+            "test-product",
+            "test-product_name",
+            1_000,
+            1000,
+            List.of("test-category"));
+
+    @Test
+    @DisplayName("주문 성공 - 잔액 및 재고 차감, 비동기 큐 전송")
+    void order() throws InterruptedException {
+        when(userRepository.findById(anyString())).thenReturn(Optional.of(testUser));
+        when(productRepository.findById(anyString())).thenReturn(Optional.of(testProduct));
+
+        orderService.order(testOrder, List.of(testOrderDetail));
+
+        Assertions.assertEquals(900_000, testUser.getUserPoint()); // 100_0000 - 100_000
+
+        verify(userRepository, times(1)).update(testUser);
+        verify(orderRepository, times(1)).save(testOrder);
+        verify(orderDetailRepository, times(1)).save(testOrderDetail);
+        verify(requestChannel, times(1)).addRequest(any(PointChannelRequest.class));
+    }
+
+    @Test
+    @DisplayName("주문 실패 - 잔액 부족")
+    void order_fail1() throws InterruptedException {
+        testUser.setUserPoint(0);
+
+        when(userRepository.findById(anyString())).thenReturn(Optional.of(testUser));
+
+        Assertions.assertThrows(InsufficientAmountException.class, () ->
+                orderService.order(testOrder, List.of(testOrderDetail)));
+
+        verify(userRepository, never()).update(any(User.class));
+        verify(productRepository, never()).findById(anyString());
+        verify(orderRepository, never()).save(any(Order.class));
+        verify(orderDetailRepository, never()).save(any(OrderDetail.class));
+        verify(requestChannel, never()).addRequest(any(PointChannelRequest.class));
+    }
+
+    @Test
+    @DisplayName("주문 실패 - 재고 부족")
+    void order_fail2() throws InterruptedException {
+        testProduct.setStock(1);
+
+        when(userRepository.findById(anyString())).thenReturn(Optional.of(testUser));
+        when(productRepository.findById(anyString())).thenReturn(Optional.of(testProduct));
+
+        Assertions.assertThrows(InsufficientQuantityException.class, () ->
+                orderService.order(testOrder, List.of(testOrderDetail)));
+
+        verify(userRepository, never()).update(any(User.class));
+        verify(orderRepository, never()).save(any(Order.class));
+        verify(orderDetailRepository, never()).save(any(OrderDetail.class));
+        verify(requestChannel, never()).addRequest(any(PointChannelRequest.class));
+    }
+
+}
