793324d7dbb3af243688dbd3c8e521b2e1ba2305
yjKang227 <hellcodedev@gmail.com>
2026-03-18 15:54:18 +0900

    feat: address model 구조 및 테스트 코드 추가

---
+++ src/main/java/com/nhnacademy/shoppingmall/address/repository/AddressRepository.java
@@ -0,0 +1,16 @@
+package com.nhnacademy.shoppingmall.address.repository;
+
+import com.nhnacademy.shoppingmall.address.domain.Address;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AddressRepository {
+    Optional<Address> findById(String addressId);
+    List<Address> findByUserId(String userId);
+    int save(Address address);
+    int update(Address address);
+    int deleteById(String addressId);
+    int deleteByUserId(String userId);
+    int countByUserId(String userId);
+}
