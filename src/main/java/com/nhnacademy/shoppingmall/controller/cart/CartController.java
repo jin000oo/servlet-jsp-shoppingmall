fe363ddb09ceff0bc327aef7690f9e76935d5849
jin000oo <wlsdud3309@naver.com>
2026-03-19 14:55:24 +0900

    refactor: 장바구니 UX 개선

--- src/main/java/com/nhnacademy/shoppingmall/controller/cart/CartController.java
+++ src/main/java/com/nhnacademy/shoppingmall/controller/cart/CartController.java
@@ -18,11 +18,17 @@ import com.nhnacademy.shoppingmall.cart.service.CartService;
 import com.nhnacademy.shoppingmall.cart.service.impl.CartServiceImpl;
 import com.nhnacademy.shoppingmall.common.mvc.annotation.RequestMapping;
 import com.nhnacademy.shoppingmall.common.mvc.controller.BaseController;
+import com.nhnacademy.shoppingmall.product.domain.Product;
+import com.nhnacademy.shoppingmall.product.repository.impl.CategoryRepositoryImpl;
+import com.nhnacademy.shoppingmall.product.repository.impl.ProductRepositoryImpl;
+import com.nhnacademy.shoppingmall.product.service.ProductService;
+import com.nhnacademy.shoppingmall.product.service.impl.ProductServiceImpl;
 import com.nhnacademy.shoppingmall.user.domain.User;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.transaction.Transactional;
@@ -32,6 +38,8 @@ import javax.transaction.Transactional;
 public class CartController implements BaseController {
 
     private final CartService cartService = new CartServiceImpl(new CartRepositoryImpl());
+    private final ProductService productService =
+            new ProductServiceImpl(new ProductRepositoryImpl(), new CategoryRepositoryImpl());
 
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) {
@@ -50,7 +58,15 @@ public class CartController implements BaseController {
             }
         }
 
+        Map<String, Product> productMap = new HashMap<>();
+
+        for (Cart cart : cartList) {
+            Product product = productService.getProduct(cart.getProductId());
+            productMap.put(cart.getProductId(), product);
+        }
+
         req.setAttribute("cartList", cartList);
+        req.setAttribute("productMap", productMap);
 
         return "shop/cart/cart";
     }
